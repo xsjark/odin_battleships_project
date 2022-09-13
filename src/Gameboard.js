3515c982edb3f960b40fe7715d1a2b5d402837c2
Ridha Watanabe <xsjark@protonmail.com>
2022-09-13 13:55:43 -0500

    obstructed vertically works

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -4,7 +4,7 @@ const Gameboard = () => {
     let board = new Array(10).fill(new Array(10).fill(0))
 
     const placeShipHorizontally = (row = 0, column = 0, size = 5) => {
-        if(shipFits(column, size)) {
+        if(shipFitsHorizontally(column, size)) {
             const ship = new Array(size).fill("a")
             const newrow = new Array(10).fill(0)
             newrow.splice(column, size, ...ship)
@@ -18,15 +18,27 @@ const Gameboard = () => {
     const shipFitsHorizontally = (column, size) => column + size <= 10;
 
     const placeShipVertically = (row, column, size = 5) => {
-            for (let i = row; i <= size+row; i++) { // off by one why
+        if(shipFitsVertically(row, size)){
+            for (let i = row; i <= size+row; i++) {
+                if(board[i][column] !== "a"){
                 const newrow = new Array(10).fill(0)
                 newrow[column] = "a"
                 board[i] = newrow
+                } else {
+                    console.log("Obstructed")
+                    break
+                }
             }
         console.log(board)
+        } else {
+            console.log("Ship doesn't fit vertically")
+        }
     }
 
     const shipFitsVertically = (row, size) =>  row + size <= 10;
+
+    const isObstructed = (row, column) => board[row][column] !== 0 
+
 
 
     return {
@@ -34,6 +46,7 @@ const Gameboard = () => {
         shipFitsHorizontally,
         placeShipVertically,
         shipFitsVertically,
+        isObstructed,
         get board() { return board}
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const Gameboard = require("./Gameboard")
 const Ship = require("./Ship")
 let b = Gameboard()
-b.placeShipVertically(4,4,4)
+b.placeShipHorizontally(2,4,4)
+b.placeShipVertically(2,4,4)
+
+
